20eb98e96e1c385b25b099bf5d4b66b3400b89f4
pierrezed <pierre.zaremba@isen.yncrea.fr>
2022-01-24 16:04:52 +0100

    modify Main Activity

--- app/src/main/java/fr/isen/pierre/zaremba/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/pierre/zaremba/androiderestaurant/HomeActivity.kt
@@ -2,10 +2,26 @@ package fr.isen.pierre.zaremba.androiderestaurant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        // get reference to button
+        val btn_click_me = findViewById(R.id.button) as Button
+        // set on-click listener
+        btn_click_me.setOnClickListener {
+            val text = "Entrees"
+            val duration = Toast.LENGTH_SHORT
+            val toast = Toast.makeText(applicationContext, text, duration)
+            toast.show()
+        }
+
+
+
     }
 }
